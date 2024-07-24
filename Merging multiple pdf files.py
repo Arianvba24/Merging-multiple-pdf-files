d88666a8969af834af0f9d0ecfb0aa1331347a4c
Arianvba24 <153762786+Arianvba24@users.noreply.github.com>
2024-07-24 21:33:45 +0200

    Update Merging multiple pdf files.py

--- Merging multiple pdf files.py
+++ Merging multiple pdf files.py
@@ -1,10 +1,10 @@
 from PyPDF2 import PdfMerger
-l1 =["certificado_udemy.pdf","pruebas_1.pdf"]
+l1 =["certificado.pdf","pruebas_1.pdf"]
 x = r"C:\Users\Cash\Downloads"
 merger = PdfMerger()
 for i in l1:
     
     merger.append(f"{x}\\{i}")
     
-merger.write(r"C:\Users\Cash\Downloads\archivo_nuevo.pdf")
+merger.write(r"C:\Users\Cash\Downloads\output.pdf")
 merger.close()
